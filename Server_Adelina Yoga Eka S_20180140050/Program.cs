08a6fdb8fe3b1385da7860a91b9a86aa11f4a053
adelina-sfr <62781888+adelina-sfr@users.noreply.github.com>
2020-10-31 14:47:32 +0700

    Add project files.

---
+++ Server_Adelina Yoga Eka S_20180140050/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.ServiceModel;
+using ServiceMtk_P1_20180140050;
+
+namespace Server_Adelina_Yoga_Eka_S_20180140050
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1_server());
+
+        }
+
+    }
+}
